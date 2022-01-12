5fb5302bf4614a0ba7e70d5307ae47264d28ef90
spences10 <spences10apps@gmail.com>
2022-01-12 10:22:48 +0000

    feat: :sparkles: add and configure tailwind + daisyui

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+  },
 
-	preprocess: [preprocess({})]
-};
+  preprocess: [preprocess({})],
+}
 
-export default config;
+export default config
