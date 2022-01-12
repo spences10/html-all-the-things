0fd8d6fc0546cec31c065e8b9b1f96783eaf6caa
spences10 <spences10apps@gmail.com>
2022-01-12 11:37:08 +0000

    feat: :sparkles: add metadata store

---
+++ src/stores/site-metadata.js
@@ -0,0 +1,13 @@
+import { client } from '$lib/graphql-client'
+import { socialsDetails } from '$lib/graphql-queries'
+import { writable } from 'svelte/store'
+
+export const socialsStore = writable({})
+
+const fetchSocials = async () => {
+  const { socialLinks } = await client.request(socialsDetails)
+
+  return socialLinks[0]
+}
+
+socialsStore.set(await fetchSocials())
