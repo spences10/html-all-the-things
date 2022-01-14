0fb08c43c7e32e186fa915fbcfdaf282f2c1304f
spences10 <spences10apps@gmail.com>
2022-01-14 10:16:17 +0000

    feat: :sparkles: add and config fathom

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,24 @@
 <script>
+  import { browser } from '$app/env'
+  import { page } from '$app/stores'
   import Footer from '$components/footer.svelte'
   import Header from '$lib/components/header.svelte'
   import Nav from '$lib/components/nav.svelte'
   import Socials from '$lib/components/socials.svelte'
+  import * as Fathom from 'fathom-client'
   import { onMount } from 'svelte'
   import { themeChange } from 'theme-change'
   import '../app.css'
 
   onMount(async () => {
     themeChange(false)
+    Fathom.load(import.meta.env.VITE_FATHOM_ID, {
+      url: import.meta.env.VITE_FATHOM_URL,
+      excludedDomains: ['localhost'],
+    })
   })
+
+  $: $page.pathname, browser && Fathom.trackPageview()
 </script>
 
 <Socials />
