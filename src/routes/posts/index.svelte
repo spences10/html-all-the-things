07e202383966f942991fbb39b50f1c8bd9e38695
spences10 <spences10apps@gmail.com>
2022-07-17 22:01:52 +0100

    style: :art: run pretty-quick

--- src/routes/posts/index.svelte
+++ src/routes/posts/index.svelte
@@ -2,7 +2,7 @@
   import { page } from '$app/stores'
   import Head from '$components/head.svelte'
   import PostCard from '$lib/components/post-card.svelte'
-  import { description,name,siteUrl } from '$lib/config'
+  import { description, name, siteUrl } from '$lib/config'
   import { client } from '$lib/graphql-client'
   import { allPosts } from '$lib/graphql-queries'
   export const load = async () => {
